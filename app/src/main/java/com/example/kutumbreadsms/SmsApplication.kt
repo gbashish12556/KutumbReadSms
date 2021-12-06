63ab2000c22c7e02dcb30a13ede992c972e78e8a
Ashish Lattukids <Ashish Gupta>
2021-12-06 22:30:53 +0530

    created smsrepo reference in application class

--- app/src/main/java/com/example/kutumbreadsms/SmsApplication.kt
+++ app/src/main/java/com/example/kutumbreadsms/SmsApplication.kt
@@ -5,6 +5,8 @@ import com.example.navigithubpr.data.source.SmsRepository
 
 class SmsApplication:Application() {
 
+    val smsRepository: SmsRepository
+        get() = ServiceLocator.provideTasksRepository(this)
 
     override fun onCreate() {
         super.onCreate()
---
+++ app/src/main/java/com/example/kutumbreadsms/util/FragmentExtension.kt
@@ -0,0 +1,5 @@
+package com.example.kutumbreadsms.util
+
+class FragmentExtension {
+}
+
