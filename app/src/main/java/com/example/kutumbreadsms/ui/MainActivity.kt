4d727b0db7900083f15c7390ae581c25f6ba98a9
Ashish Lattukids <Ashish Gupta>
2021-12-08 10:38:03 +0530

    completed notification part

--- app/src/main/java/com/example/kutumbreadsms/ui/MainActivity.kt
+++ app/src/main/java/com/example/kutumbreadsms/ui/MainActivity.kt
@@ -15,11 +15,21 @@
  */
 package com.example.kutumbreadsms.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.kutumbreadsms.R
+import com.example.kutumbreadsms.data.SmsData
 
 /**
  * Main activity for the todoapp. Holds the Navigation Host Fragment and the Drawer, Toolbar, etc.
@@ -29,6 +39,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+        if(intent != null && intent.extras != null){
+            var smsData:SmsData = intent.extras!!.getSerializable("smsData") as SmsData
+            var navController:NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+            navController.navigate(R.id.action_messageListFragment2_to_mesageDetailFragment2, Bundle().apply {
+                putSerializable("smsData", smsData)
+            })
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannel(){
+        val channel = NotificationChannel("SmsChannel",
+            "Sms Channel",NotificationManager.IMPORTANCE_HIGH)
+        channel.lightColor = Color.GREEN
+        channel.description = "Sms Channel"
+        val notificationManager: NotificationManager =
+            this.getSystemService<NotificationManager>(
+                NotificationManager::class.java
+            )
+        notificationManager.createNotificationChannel(channel)
     }
 
 }
--- app/src/main/java/com/example/kutumbreadsms/ui/MesageDetailFragment.kt
+++ app/src/main/java/com/example/kutumbreadsms/ui/MesageDetailFragment.kt
@@ -5,17 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.kutumbreadsms.R
+import com.example.kutumbreadsms.data.SmsData
 
 
 class MesageDetailFragment : Fragment() {
+
+    private var sender: TextView? = null
+    private var message: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mesage_detail, container, false)
+        var view:View =  inflater.inflate(R.layout.fragment_mesage_detail, container, false)
+        sender = view.findViewById(R.id.sender)
+        message = view.findViewById(R.id.msessage)
+        return view;
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if(arguments !=null && requireArguments().getSerializable("smsData") != null){
+            var smsData:SmsData = requireArguments().getSerializable("smsData") as SmsData
+            sender!!.setText(smsData.sender)
+            message!!.setText(smsData.message)
+        }
+    }
 }
