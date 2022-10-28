182da631de022f5a0607c432808a858e43625b9a
Ashish Gupta <ashish.gupta@nobroker.in>
2022-10-28 22:42:19 +0530

    WIP

--- app/src/main/java/com/example/kutumbreadsms/SmsApplication.kt
+++ app/src/main/java/com/example/kutumbreadsms/SmsApplication.kt
@@ -2,7 +2,9 @@ package com.example.kutumbreadsms
 
 import android.app.Application
 import com.example.navigithubpr.data.source.SmsRepository
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp()
 class SmsApplication:Application() {
 
     val smsRepository: SmsRepository
